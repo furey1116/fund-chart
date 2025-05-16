03f45cddcf216e4b0347e47e0958b0a8a7c2d6f2
furey1116 <furey1116@163.com>
2025-05-16 14:46:09 +0800

    icon added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,22 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata = {
   title: '基金净值及网格操作系统',
   description: '一个基于Next.js的基金净值查询、曲线绘制、网格操作系统',
+  icons: {
+    icon: [
+      { url: '/icons/favicon.ico' },
+      { url: '/icons/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/icons/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: [
+      { url: '/icons/apple-touch-icon.png' },
+    ],
+    other: [
+      {
+        rel: 'manifest',
+        url: '/icons/site.webmanifest',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
